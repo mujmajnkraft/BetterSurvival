e0508eb584affe1a845aa7a5af0f3dcc33347875
Block-Vader <hrosik.pm@gmail.com>
2023-03-05 00:11:52 +0100

    Added an extra check to prevent errors with an unsupported IaF version

--- src/main/java/com/mujmajnkraft/bettersurvival/recipe/RecipeCondition.java
+++ src/main/java/com/mujmajnkraft/bettersurvival/recipe/RecipeCondition.java
@@ -3,7 +3,7 @@ package com.mujmajnkraft.bettersurvival.recipe;
 import java.util.function.BooleanSupplier;
 
 import com.google.gson.JsonObject;
-
+import com.mujmajnkraft.bettersurvival.BetterSurvival;
 import com.mujmajnkraft.bettersurvival.config.ForgeConfigHandler;
 import net.minecraft.util.JsonUtils;
 import net.minecraftforge.common.crafting.IConditionFactory;
@@ -15,6 +15,7 @@ public class RecipeCondition implements IConditionFactory{
 	@Override
 	public BooleanSupplier parse(JsonContext context, JsonObject json) {
 		String modID = JsonUtils.getString(json, "mod_id");
+		if (modID.matches("iceandfire")) return () -> BetterSurvival.isIafLoaded; //Added an extra check to prevent errors with an unsupported IaF version
 		boolean isModLoaded = Loader.isModLoaded(modID) && ForgeConfigHandler.materials.moddedMaterials;
 		return () -> isModLoaded;
 	}
